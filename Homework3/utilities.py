644788b7ad373b92407b9886db0e02ff66cec38a
rossetl <rosset.lorenzo@gmail.com>
2022-06-04 18:11:52 +0200

    update

---
+++ Homework3/utilities.py
@@ -0,0 +1,137 @@
+import random
+import torch
+import numpy as np
+import gym
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
+from collections import deque
+from torch import nn
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque(maxlen=capacity) # Define a queue with maxlen "capacity"
+
+    def push(self, state, action, next_state, reward):
+        self.memory.append((state, action, next_state, reward))
+    
+    def sample(self, batch_size):
+        batch_size = min(batch_size, len(self)) # Get all the samples if the requested batch_size is higher than the number of sample currently in the memory
+        return random.sample(self.memory, batch_size) # Randomly select "batch_size" samples
+
+    def __len__(self):
+        return len(self.memory) # Return the number of samples currently stored in the memory
+    
+class DQN(nn.Module):
+
+    def __init__(self, state_space_dim, action_space_dim):
+        super().__init__()
+
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=state_space_dim, out_features=128),
+            nn.Tanh(),
+            nn.Linear(in_features=128, out_features=128),
+            nn.Tanh(),
+            nn.Linear(in_features=128, out_features=action_space_dim)
+                )
+
+    def forward(self, x):
+        return self.linear(x)
+    
+    
+def choose_action_epsilon_greedy(net, state, epsilon, device):
+    
+    if epsilon > 1 or epsilon < 0:
+        raise Exception('The epsilon value must be between 0 and 1')
+                
+    # Evaluate the network output from the current state
+    with torch.no_grad():
+        net.eval()
+        state = torch.tensor(state, dtype=torch.float32, device=device) # Convert the state to tensor
+        net_out = net(state)
+
+    # Get the best action (argmax of the network output)
+    best_action = int(net_out.argmax())
+    # Get the number of possible actions
+    action_space_dim = net_out.shape[-1]
+
+    # Select a non optimal action with probability epsilon, otherwise choose the best action
+    if random.random() < epsilon:
+        # List of non-optimal actions
+        non_optimal_actions = [a for a in range(action_space_dim) if a != best_action]
+        # Select randomly
+        action = random.choice(non_optimal_actions)
+    else:
+        # Select best action
+        action = best_action
+        
+    return action, net_out.cpu().numpy()
+
+def choose_action_softmax(net, state, temperature, device):
+    
+    if temperature < 0:
+        raise Exception('The temperature value must be greater than or equal to 0 ')
+        
+    # If the temperature is 0, just select the best action using the eps-greedy policy with epsilon = 0
+    if temperature == 0:
+        return choose_action_epsilon_greedy(net, state, 0)
+    
+    # Evaluate the network output from the current state
+    with torch.no_grad():
+        net.eval()
+        state = torch.tensor(state, dtype=torch.float32, device=device)
+        net_out = net(state)
+
+    # Apply softmax with temp
+    temperature = max(temperature, 1e-8) # set a minimum to the temperature for numerical stability
+    softmax_out = nn.functional.softmax(net_out / temperature, dim=0).cpu().numpy()
+                
+    # Sample the action using softmax output as mass pdf
+    all_possible_actions = np.arange(0, softmax_out.shape[-1])
+    action = np.random.choice(all_possible_actions, p=softmax_out) # this samples a random element from "all_possible_actions" with the probability distribution p (softmax_out in this case)
+    
+    return action, net_out.cpu().numpy()
+
+def update_step(policy_net, target_net, replay_mem, gamma, optimizer, loss_fn, batch_size, device):
+        
+    # Sample the data from the replay memory
+    batch = replay_mem.sample(batch_size)
+    batch_size = len(batch)
+
+    # Create tensors for each element of the batch
+    states      = torch.tensor([s[0] for s in batch], dtype=torch.float32, device=device)
+    actions     = torch.tensor([s[1] for s in batch], dtype=torch.int64, device=device)
+    rewards     = torch.tensor([s[3] for s in batch], dtype=torch.float32, device=device)
+
+    # Compute a mask of non-final states (all the elements where the next state is not None)
+    non_final_next_states = torch.tensor([s[2] for s in batch if s[2] is not None], dtype=torch.float32, device=device) # the next state can be None if the game has ended
+    non_final_mask = torch.tensor([s[2] is not None for s in batch], dtype=torch.bool, device=device)
+
+    # Compute all the Q values (forward pass)
+    policy_net.train()
+    q_values = policy_net(states)
+    # Select the proper Q value for the corresponding action taken Q(s_t, a)
+    state_action_values = q_values.gather(1, actions.unsqueeze(1))
+
+    # Compute the value function of the next states using the target network V(s_{t+1}) = max_a( Q_target(s_{t+1}, a)) )
+    with torch.no_grad():
+      target_net.eval()
+      q_values_target = target_net(non_final_next_states)
+    next_state_max_q_values = torch.zeros(batch_size, device=device)
+    next_state_max_q_values[non_final_mask] = q_values_target.max(dim=1)[0]
+
+    # Compute the expected Q values
+    expected_state_action_values = rewards + (next_state_max_q_values * gamma)
+    expected_state_action_values = expected_state_action_values.unsqueeze(1) # Set the required tensor shape
+
+    # Compute the Huber loss
+    loss = loss_fn(state_action_values, expected_state_action_values)
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    # Apply gradient clipping (clip all the gradients greater than 2 for training stability)
+    nn.utils.clip_grad_norm_(policy_net.parameters(), 2)
+    optimizer.step()
+    
+
